a5da765f43d64dd8c61fcd92e137a23bf4cfe3db
ega-wave <health@ega-wave.com>
2024-05-05 22:53:15 +0900

    add License Notice

--- c/1/cat.c
+++ c/1/cat.c
@@ -1,6 +1,27 @@
+/**
+cat.s -- plain cat(1) read from stdin and write to stdout
 
-// plain-cat(1) - copy stdin to stdout
-// using system call read(2)/write(2)
+Copyright (C) 2024 Yoshitaka Egawa.
+
+This program is free software: you can redistribute it and/or 
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either 
+version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License 
+along with this program.
+If not, see <https://www.gnu.org/licenses/>.
+**/
+
+/* Written by Yoshitaka Egawa. */
+
+/* plain-cat(1) - copy stdin to stdout */
+/* using system call read(2)/write(2)  */
 
 #include <unistd.h> // for read(), write()
 #include <stdio.h>  // for fputs(), fprintf()
