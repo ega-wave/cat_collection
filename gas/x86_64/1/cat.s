aed002cc6f3c72a68a3499a6979f2389d5444174
ega-wave <health@ega-wave.com>
2024-11-08 00:07:22 +0900

    use %ebx which holds a value of 1

--- gas/x86_64/1/cat.s
+++ gas/x86_64/1/cat.s
@@ -10,17 +10,21 @@
 	.globl _start
 	.type  _start, @function
 _start:
+	pushq	%rbx
 	pushq	%rsi
 	pushq	%rdi
 	pushq	%rdx
 	leaq	buf(%rip), %rsi
+	movl	$1, %ebx	/* ebx = 1 */
 				/* fall through */
 /*
-rax	rdx	rdi	rsi
+rax	rbx	rdx	rdi	rsi
+	u/
+				u/
 			u/
 		u/
-	u/
-			/b
+				/b
+	/1
 b : (void*)buf
 u : (uninitialized value)
 */
@@ -32,14 +36,15 @@ u : (uninitialized value)
 	xorl	%eax, %eax    # syscall_id = 0 (read)
 	syscall               # rax = read(edi, rsi, edx);
 	testl	%eax, %eax    # eax == 0
-	je	.finish       # if true goto .finish
+	je	.exit0        # if true goto .exit0
+				/* else fall through */
 /*
-rax	rdx	rdi	rsi
-	/13
-		u/0
+rax	rbx	rdx	rdi	rsi
+		/13
+			u/0
 u/0
-/rc	13/	0/	b/
-rc/				
+0/rc		13/	0/	b/
+rc/
 
 b : (void*)buf
 13: buf_size(=131072)
@@ -48,34 +53,48 @@ rc: read_count
 
 .write:
 	movl	%eax, %edx    # edx = eax
-	incl	%edi          # edi = 1 (fd)
-	movl	%edi, %eax    # syscall_id = 1 (write)
+	movl	%ebx, %edi    # edi = 1 (fd)
+	movl	%ebx, %eax    # syscall_id = 1 (write)
 	syscall               # rax = write(edi, rsi, rdx)
 	cmpl	%eax, %edx    # eax == edx
 	je	.loop1        # if true goto .loop1
-	movb	$1, %al       # else al = 1 (and fall through)
+				/* else fall-through */
 /*
-rax	rdx	rdi	rsi
-rc/	/rc
-		0/1
-/1		1/
-/wc	rc/	1/	b/
-wc/	rc/
-/1
+rax	rbx	rdx	rdi	rsi
+rc/		/rc
+	1/		/1
+/1	1/
+1/wc		rc/	1/	b/
+wc/		rc/
 
 b : (void*)buf
 rc: read_count
 wc: write_count
 */
 
+.exit1:
+	movl	$1, %eax	/* eax = 1 */
+	jmp	.finish
+.exit0:
+	xorl	%eax, %eax	/* eax = 0 */
+				/* fall through */
 .finish:
 	popq	%rdx
 	popq	%rdi
 	popq	%rsi
+	popq	%rbx
+	pushq	%rdi
 .exit:
-	movb %al, %dil # return code
+	movl %eax,%edi # return code
 	movb $60, %al  # syscall_id = 60 (exit)
 	syscall        # exit(al) # 0 or 1
+	popq	%rdi
+/*
+rax	rbx	rdx	rdi	rsi
+0/			/0
+/60
+60/			0/
+*/
 	.size   _start, .-_start
 
 	.globl	buf
