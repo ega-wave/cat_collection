873c75e79f437e80356b325d17cb91ade291f706
ega-wave <health@ega-wave.com>
2024-12-22 23:19:32 +0900

    delete an unneccesary mov

--- gas/x86_64/3/cat.2.s
+++ gas/x86_64/3/cat.2.s
@@ -10,11 +10,22 @@ int main()
 _start:
 	pushq	%rbp
 	pushq	%r8
-	leaq	read_buf(%rip), %rbp
-	leaq	write_buf(%rip), %r8
 	pushq	%rbx
 	pushq	%rcx
+	leaq	read_buf(%rip), %rbp
+	leaq	write_buf(%rip), %r8
 	movl	$1, %ebx      /* ebx = 1 */
+/*
+rax	rbx	rcx	rdx	rdi	rsi	rbp	rsp	r8
+	1	loop	13	fd	buf	rd_buf		wr_buf
+						u/		
+								u/
+	u/							
+		u/						
+						/rb		
+								/wb
+	/1							
+*/
 
 .loop:
 .read:
@@ -26,10 +37,26 @@ _start:
 	testq	%rax, %rax
 	je	.exit0        /* if true, goto .exit0 (with eax=0) */
                               /*    else, fall through */
+/*
+rax	rbx	rcx	rdx	rdi	rsi	rbp	rsp	r8
+	1	loop	13	fd	buf	rd_buf		wr_buf
+				u/0				
+	/13							
+					/rb	rb/		
+u/0								
+0/cr	13/			0/	rb/			
+cr/								
+*/
 
 .memcpy_initialize:
 	xorl	%ecx, %ecx
 	movq	%r8, %rsi
+/*
+rax	rbx	rcx	rdx	rdi	rsi	rbp	rsp	r8
+	1	loop	13	fd	buf	rd_buf		wr_buf
+		u/0						
+					/wb			wb/
+*/
 
 .memcpy:
 	movb	0(%rbp,%rcx), %dl /* dl = rbp[rcx] */
@@ -37,15 +64,32 @@ _start:
 	incq	%rcx
 	cmpq	%rax, %rcx
 	jne	.memcpy
+/*
+rax	rbx	rcx	rdx	rdi	rsi	rbp	rsp	r8
+	1	loop	13	fd	buf	rd_buf		wr_buf
+		lc/	/rb[lc]			rb/		
+		lc/	rb[lc]/		wb/			
+		lc/lc+1						
+cr/		lc/						
+*/
 
 .write:
 	movl	%ebx, %edi    /* edi = 1 (fd) */
-	movq	%r8,  %rsi    /* rsi = r8 (=write_buf) */
 	movq	%rcx, %rdx
 	movl	%ebx, %eax    /* syscall_id = 1 (write) */
 	syscall               /* rax = write(edi, rsi, rdx) */
 	cmpq	%rax, %rdx
 	je	.read
+/*
+rax	rbx	rcx	rdx	rdi	rsi	rbp	rsp	r8
+	1	loop	13	fd	buf			wr_buf
+	1/			/1				
+					/wb			wb/
+		cr/	/cr					
+/1	1/							
+/cw			cr/	1/	wb/			
+cw/			cr/					
+*/
 
 .exit1:
 	movl	%ebx, %eax    /* eax = 1 */
